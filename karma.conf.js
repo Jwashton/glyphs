eefa9d74a6d6980f89dcdfb2006a4b7b299fdef3
William Ashton <William@AshtonFam.org>
2016-05-29 14:28:39 -0400

    Setup ESLint

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests.webpack.js': ['webpack', 'sourcemap']
+      'tests.webpack.js': ['eslint', 'webpack', 'sourcemap']
     },
 
 
